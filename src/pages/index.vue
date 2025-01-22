7316143872dd4b36b7e61d6cfd8e0931877e187c
AyumiHeiress <ayumi.harun@gmail.com>
2025-01-22 00:16:31 -0600

    end of chapter2 structure and routing, unplugin-vue-router make dynamical imports for routes

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,6 +5,6 @@ import { RouterLink } from 'vue-router'
 <template>
   <div>
     <h1>Home page</h1>
-    <RouterLink to="/projects">go to projects</RouterLink>
+    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">go to project 1</RouterLink>
   </div>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,34 +1,9 @@
-import HomeView from '@/views/HomeView.vue'
-import { h } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      // using DYNAMIC IMPORT we only download the file once is needed
-      component: () => import('@/views/ProjectsView.vue'),
-    },
-    {
-      // projects/1
-      path: '/projects/:id',
-      name: 'single-project',
-      component: () => import('@/views/SingleProjectView.vue'),
-    },
-    {
-      // catch all routes, it can be preceded from other module
-      path: '/:catchAll(.*)*',
-      name: 'NotFound',
-      component: h('p', { style: 'color: red;' }, '404 Not Found'),
-    },
-  ],
+  routes,
 })
 
 export default router
